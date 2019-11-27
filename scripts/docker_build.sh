71476a985c44fe7d1ca1d3926f66bff4dee40efb
wassen <k-asayama@outlook.com>
2019-11-27 22:04:54 +0900

    change environment variables

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ue
-docker run -e keymap=wassen -e keyboard=ergodox_ez --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware
+docker run -e KEYMAP=wassen -e KEYBOARD=ergodox_ez --rm -v $('pwd'):/qmk_firmware:rw edasque/qmk_firmware
 
